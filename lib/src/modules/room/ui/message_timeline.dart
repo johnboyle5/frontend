7d32bb691c65b3a326497338db845629db8e3b9d
Jaemin Jo <44039707+91jaeminjo@users.noreply.github.com>
2026-03-26 16:35:50 -0500

    feat(room): rewrite MessageTimeline with anchored scroll

--- lib/src/modules/room/ui/message_timeline.dart
+++ lib/src/modules/room/ui/message_timeline.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:soliplex_agent/soliplex_agent.dart';
+import 'package:flutter/rendering.dart';
+import 'package:soliplex_agent/soliplex_agent.dart' hide State;
 
 import '../execution_tracker.dart';
 import '../run_id_resolver.dart';
 import 'message_tile.dart';
 import 'room_welcome.dart';
+import 'scroll/anchored_scroll_controller.dart';
+import 'scroll/scroll_to_bottom.dart';
 import 'streaming_tile.dart';
 
-class MessageTimeline extends StatelessWidget {
+class MessageTimeline extends StatefulWidget {
   const MessageTimeline({
     super.key,
     required this.messages,
@@ -17,7 +20,6 @@ class MessageTimeline extends StatelessWidget {
     this.room,
     this.onSuggestionTapped,
     this.onFeedbackSubmit,
-    this.scrollController,
   });
 
   final List<ChatMessage> messages;
@@ -28,43 +30,205 @@ class MessageTimeline extends StatelessWidget {
   final void Function(String suggestion)? onSuggestionTapped;
   final void Function(String runId, FeedbackType feedback, String? reason)?
       onFeedbackSubmit;
-  final ScrollController? scrollController;
+
+  @override
+  State<MessageTimeline> createState() => _MessageTimelineState();
+}
+
+class _MessageTimelineState extends State<MessageTimeline> {
+  late final AnchoredScrollController _scrollController;
+  late final ScrollToBottomController _scrollToBottomController;
+
+  final Map<String, GlobalKey> _messageKeys = {};
+  String? _lastUserMessageId;
+  String? _scrollTargetId;
+  bool _needsInitialScroll = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = AnchoredScrollController();
+    _scrollToBottomController = ScrollToBottomController();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void didUpdateWidget(MessageTimeline oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    final lastUserMsg = _findLastUserMessage(widget.messages);
+    if (lastUserMsg != null && lastUserMsg.id != _lastUserMessageId) {
+      _lastUserMessageId = lastUserMsg.id;
+      _needsInitialScroll = false;
+      _scrollTargetId = lastUserMsg.id;
+      _scrollToMessage(lastUserMsg.id, animate: true);
+    } else if (_scrollTargetId != null) {
+      _scrollToMessage(_scrollTargetId!);
+    }
+
+    if (widget.streamingState == null && oldWidget.streamingState != null) {
+      _scrollTargetId = null;
+    }
+
+    final activeIds = widget.messages.map((m) => m.id).toSet();
+    _messageKeys.removeWhere((id, _) => !activeIds.contains(id));
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    _scrollToBottomController.dispose();
+    super.dispose();
+  }
+
+  TextMessage? _findLastUserMessage(List<ChatMessage> messages) {
+    for (var i = messages.length - 1; i >= 0; i--) {
+      final m = messages[i];
+      if (m is TextMessage && m.user == ChatUser.user) return m;
+    }
+    return null;
+  }
+
+  void _scrollToMessage(String messageId, {bool animate = false}) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!_scrollController.hasClients) return;
+
+      if (_tryEnsureVisible(messageId, animate: animate)) return;
+
+      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (!_scrollController.hasClients) return;
+        _tryEnsureVisible(messageId, animate: animate);
+      });
+    });
+  }
+
+  bool _tryEnsureVisible(String messageId, {bool animate = false}) {
+    final key = _messageKeys[messageId];
+    if (key?.currentContext == null) return false;
+
+    final renderObject = key!.currentContext!.findRenderObject()!;
+    final viewport = RenderAbstractViewport.of(renderObject);
+    final target = viewport.getOffsetToReveal(renderObject, 0.0).offset - 8;
+
+    _scrollController.setAnchor(target);
+
+    if (animate) {
+      _scrollController.animateTo(
+        target,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOutCubic,
+      );
+    } else {
+      _scrollController.jumpTo(target);
+    }
+    return true;
+  }
+
+  void _scrollToBottom() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!_scrollController.hasClients) return;
+      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+    });
+  }
+
+  void _onScroll() {
+    _scrollToBottomController.updateScrollPosition(_scrollController);
+  }
+
+  void _onScrollToBottom() {
+    _scrollTargetId = null;
+    _scrollController.clearAnchor();
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOutCubic,
+    );
+    _scrollToBottomController.hide();
+  }
+
+  bool _onScrollNotification(ScrollNotification notification) {
+    if (notification is ScrollStartNotification &&
+        notification.dragDetails != null &&
+        _scrollTargetId != null) {
+      _scrollTargetId = null;
+    }
+    return false;
+  }
+
+  GlobalKey _keyFor(String id) {
+    return _messageKeys.putIfAbsent(id, () => GlobalKey());
+  }
 
   @override
   Widget build(BuildContext context) {
-    final hasStreaming = streamingState != null;
-    final itemCount = messages.length + (hasStreaming ? 1 : 0);
+    final hasStreaming = widget.streamingState != null;
+    final itemCount = widget.messages.length + (hasStreaming ? 1 : 0);
+
     if (itemCount == 0) {
       return RoomWelcome(
-        room: room,
-        onSuggestionTapped: onSuggestionTapped,
+        room: widget.room,
+        onSuggestionTapped: widget.onSuggestionTapped,
         fallback: _emptyFallback(context),
       );
     }
-    final runIdMap = buildRunIdMap(messages, messageStates);
-    return ListView.builder(
-      controller: scrollController,
-      reverse: true,
-      padding: const EdgeInsets.all(16),
-      itemCount: itemCount,
-      itemBuilder: (context, reversedIndex) {
-        if (hasStreaming && reversedIndex == 0) {
-          return StreamingTile(
-            key: const ValueKey('streaming'),
-            streamingState: streamingState!,
-            executionTracker: executionTracker,
-          );
-        }
-        final messageIndex = hasStreaming
-            ? messages.length - reversedIndex
-            : messages.length - 1 - reversedIndex;
-        return MessageTile(
-          key: ValueKey(messages[messageIndex].id),
-          message: messages[messageIndex],
-          runId: runIdMap[messages[messageIndex].id],
-          onFeedbackSubmit: onFeedbackSubmit,
-        );
-      },
+
+    if (_needsInitialScroll) {
+      _needsInitialScroll = false;
+      _lastUserMessageId = _findLastUserMessage(widget.messages)?.id;
+      _scrollToBottom();
+    }
+
+    final runIdMap = buildRunIdMap(widget.messages, widget.messageStates);
+
+    return Stack(
+      children: [
+        NotificationListener<ScrollNotification>(
+          onNotification: _onScrollNotification,
+          child: CustomScrollView(
+            controller: _scrollController,
+            slivers: [
+              SliverPadding(
+                padding: const EdgeInsets.all(16),
+                sliver: SliverList.builder(
+                  itemCount: itemCount,
+                  itemBuilder: (context, index) {
+                    if (hasStreaming && index == widget.messages.length) {
+                      return Padding(
+                        key: const ValueKey('streaming'),
+                        padding: const EdgeInsets.only(bottom: 16),
+                        child: StreamingTile(
+                          streamingState: widget.streamingState!,
+                          executionTracker: widget.executionTracker,
+                        ),
+                      );
+                    }
+                    final message = widget.messages[index];
+                    return Padding(
+                      key: _keyFor(message.id),
+                      padding: const EdgeInsets.only(bottom: 16),
+                      child: MessageTile(
+                        message: message,
+                        runId: runIdMap[message.id],
+                        onFeedbackSubmit: widget.onFeedbackSubmit,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          right: 16,
+          bottom: 16,
+          child: ScrollToBottomButton(
+            controller: _scrollToBottomController,
+            onPressed: _onScrollToBottom,
+          ),
+        ),
+      ],
     );
   }
 
